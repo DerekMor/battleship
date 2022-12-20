024d794abd485a45f5acbcb1dceee7b2350e9546
DerekMor <derekmoriarty24@gmail.com>
2022-12-20 22:30:58 +0000

    Add welcome message and ask for users name

--- run.py
+++ run.py
@@ -1,2 +1,25 @@
-def main ():
-    #Welcome message
+
+
+
+def main():
+    """
+    Main method that starts the game and calls all other methods
+    """
+    print("Welcome to Battleship!\n")
+    name = input("Please enter your name:\n")
+    # size grid
+    # number of ships
+    # create grid
+    # place ships
+    # play game {
+    # dislay game grid
+    # pick a coordinate
+    # check for him or miss and display message
+    # if hit check for win
+    # generate comp guess
+    # if hit check for win
+    # when won diplay message
+    # play again?
+
+
+main()
